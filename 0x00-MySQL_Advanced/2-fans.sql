c9fda3aba2c773b0f0d4bf33582c211a4ac5c04a
NiniolaX <loveafinni16@gmail.com>
2024-09-16 15:14:28 +0100

    Updated comment

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- Script ranks the country of origin of bands, ordered by the number of (non-unique) fans
+-- Script ranks the country of origin of bands in 'metal_bands.sql', ordered by the number of (non-unique) fans
 -- Group bands by country and sum of fans
 SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
