83466d4c2634afd2687556bc03a0d82c58303b39
NiniolaX <loveafinni16@gmail.com>
2024-09-17 17:21:14 +0100

    Added script that creates a division function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,18 @@
+-- Script creates a function that computes and returns the division of two numbers, but returns 0 if the second number is 0
+-- Query that creates function
+DROP FUNCTION SafeDiv;
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+    RETURNS FLOAT DETERMINISTIC
+    BEGIN
+        DECLARE result FLOAT;
+        IF b = 0 THEN
+            SET result = 0;
+        ELSE
+            SET result = a / b;
+        END IF;
+        RETURN result;
+    END $$
+
+DELIMITER ;
