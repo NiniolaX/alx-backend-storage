dc2de323a0316804a3f0eaaa835263cf9a91d1df
NiniolaX <loveafinni16@gmail.com>
2024-09-25 23:29:27 +0100

    Removed redundant class

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,11 +2,10 @@
 """
 Contains a function that lists all the collections in a mongodb database
 """
-from pymongo.collection import Collection
 from typing import List
 
 
-def list_all(mongo_collection: Collection) -> List:
+def list_all(mongo_collection) -> List:
     """ Lists all documents in a collection """
     if mongo_collection.count_documents == 0:
         return []
