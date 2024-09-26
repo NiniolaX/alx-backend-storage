4a81cf5cde9ec276f2bf06a911ff530a9ec24e35
NiniolaX <loveafinni16@gmail.com>
2024-09-26 10:15:16 +0100

    Modified script to do nothing if no match is found

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """
-Contains a function that changes all the topics of a school document based on its name
+Contains a function that changes all the topics of a school document based on
+its name.
 """
 from typing import List
 
 
-def update_topics(mongo_collection, name: str, topics: List[str]):
+def update_topics(mongo_collection, name: str, topics: List[str]) -> None:
     """ Changes all the topics of a school document based on its name
     Args:
         mongo_collection (PymongoCollectionObject): pymongo ollection object
         name (str): school name to update
         topics (list of strings): List of topics approached in the school
     """
-    result = mongo_collection.update_many( { 'name': name }, { '$set': { 'topics': str(topics) } })
+    result = mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+        )
